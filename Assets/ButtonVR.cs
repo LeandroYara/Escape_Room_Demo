7e4029c7b127b445da92534073142be2f786de7a
Nicolás Abondano <69647862+LeandroYara@users.noreply.github.com>
2023-02-22 18:29:28 -0500

    Prueba de laberinto y boton

--- Assets/ButtonVR.cs
+++ Assets/ButtonVR.cs
@@ -11,32 +11,30 @@ public class ButtonVR : MonoBehaviour
     public UnityEvent onPress;
     public UnityEvent onRelease;
     GameObject presser;
-    bool IsPressed;
+    bool isPressed;
     // Start is called before the first frame update
     void Start()
     {
-        IsPressed = false;
+        isPressed = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!IsPressed)
+        if (!isPressed)
         {
-            Button.transform.localPosition= new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
             Button.GetComponent<Renderer>().material.color = Color.green;
             presser = other.gameObject;
             onPress.Invoke();
-            IsPressed = true;
+            isPressed = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other == presser)
         {
-            Button.transform.localPosition = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
             Button.GetComponent<Renderer>().material.color = Color.red;
             onRelease.Invoke();
-            IsPressed   = false;
+            isPressed = false;
         }
     }
 }
